a6c4e3cc7afacf7194bb712331d6ae0424c64a1f
dta <daniel.takacs@sofiadigital.com>
2024-02-15 11:33:52 +0200

    Prepare for deployment

--- server/index.js
+++ server/index.js
@@ -8,15 +8,23 @@ const http = require('http');
 const server = http.createServer(app);
 const socketIo = require('socket.io');
 const PORT = process.env.PORT || 4000;
-console.log(process.env.PORT)
 const cors = require('cors');
+const path = require('path');
+
+// Serve static files from the Vite-built frontend
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// Handle SPA routing by redirecting all non-API requests to the index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+});
+
 app.use(cors());
 /* require('crypto').randomBytes(32, function(err, buffer) {
     var token = buffer.toString('hex');
     console.log(token);
   }); */
 app.use(express.json()); // Middleware for parsing JSON bodies
-
 // Use the authentication routes
 app.use('/auth', authRoutes);
 connectDB();
